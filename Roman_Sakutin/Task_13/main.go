a884c1269159a109b58e1e33084f16a565c93997
yuldash13 <yuldash.nazarov@icloud.com>
2024-11-03 17:09:16 +0300

    Исправлен

--- Roman_Sakutin/Task_13/main.go
+++ Roman_Sakutin/Task_13/main.go
@@ -2,210 +2,164 @@ package main
 
 import (
 	"fmt"
+	"strings"
+)
+
+const (
+	rubUsd = 1 / 97.0
+	rubEur = 1 / 105.0
+	usdRub = 97.0
+	usdEur = 1 / 1.1
+	eurRub = 105.0
+	eurUsd = 1.1
 )
 
 func main() {
 	var (
-		quantity       float64
-		money          string
-		menu           int
+		quantity, sum                float64
+		answer, transfer1, transfer2 string
+		menu                         int
+
 		rubblesBalance = 1000.0
 		dollarsBalance = 100.0
 		euroBalance    = 5.0
 	)
+
+	bank := map[string]float64{
+		"rub": rubblesBalance,
+		"usd": dollarsBalance,
+		"eur": euroBalance,
+	}
+
+	course := map[string]map[string]float64{
+		"rub": {
+			"rub": 1,
+			"usd": rubUsd,
+			"eur": rubEur,
+		},
+		"usd": {
+			"rub": usdRub,
+			"usd": 1,
+			"eur": usdEur,
+		},
+		"eur": {
+			"rub": eurRub,
+			"usd": eurUsd,
+			"eur": 1,
+		},
+	}
 
 	fmt.Println("Здравствуйте, чем могу помочь?")
 
 mainLoop:
 	for {
 		fmt.Println("Меню:\n1)Баланс\n2)Внести деньги\n3)Снять деньги\n4)Перевод валюты\n5)Выход")
-		fmt.Println()
-		fmt.Scan(&menu)
+		fmt.Scanln(&menu)
 		switch menu {
 		case 1:
 			fmt.Printf("У вас на счету:\n%v рублей\n%v долларов\n%v евро\n", rubblesBalance, dollarsBalance, euroBalance)
-			fmt.Println()
 		case 2:
+		deposit:
 			for {
-				fmt.Println("Какую валюту вы хотите внести?\n1)Рубли\n2)Доллары\n3)Евро\n4)Назад")
-				fmt.Println()
-				fmt.Scan(&money)
-				if money == "1" {
-					fmt.Println("Сколько вы хотите ввести?")
-					fmt.Scan(&quantity)
-					if quantity < 0 {
-						fmt.Println("Вы ввели неверное значение.")
-						fmt.Println()
-					} else {
-						rubblesBalance += quantity
-						fmt.Printf("На вашем счету: %v рублей.\n", rubblesBalance)
-						fmt.Println()
-					}
-				} else if money == "2" {
-					fmt.Println("Сколько вы хотите ввести?")
-					fmt.Scan(&quantity)
-					if quantity < 0 {
-						fmt.Println("Вы ввели неверное значение.")
-						fmt.Println()
-					} else {
-						dollarsBalance += quantity
-						fmt.Printf("На вашем счету: %v долларов.\n", dollarsBalance)
-						fmt.Println()
-					}
-				} else if money == "3" {
-					fmt.Println("Сколько вы хотите ввести?")
-					fmt.Scan(&quantity)
-					if quantity < 0 {
-						fmt.Println("Вы ввели неверное значение.")
-						fmt.Println()
-					} else {
-						euroBalance += quantity
-						fmt.Printf("На вашем счету: %v евро.\n", euroBalance)
-						fmt.Println()
-					}
-				} else if money == "4" {
-					break
-				} else {
-					fmt.Println("Вы ввели что-то неверно, попробуйте еще раз.")
-					fmt.Println()
+				fmt.Println("Вы можете ввести следующие валюты: Rub, Usd, Eur. Какую валюту вы хотите внести?")
+				fmt.Scanln(&transfer1)
+
+				transfer1 = strings.ToLower(transfer1)
+
+				if _, ok := bank[transfer1]; !ok {
+					fmt.Println("Вы ввели данные не верно.")
+					continue deposit
+				}
+
+				fmt.Println("Сколько вы хотите ввести?")
+				fmt.Scanln(&quantity)
+
+				if quantity < 0 {
+					fmt.Println("Вы ввели неверное значение.")
+					continue deposit
+				}
+
+				bank[transfer1] += quantity
+
+				fmt.Printf("На вашем счету: %v %s.\n", float64(int(bank[transfer1]*100))/100.0, transfer1)
+
+				fmt.Println("Вернуться в главное меню? (Да/Нет)")
+				fmt.Scanln(&answer)
+
+				answer = strings.ToLower(answer)
+
+				if answer == "да" {
+					break deposit
 				}
 			}
 		case 3:
+		depositOut:
 			for {
-				fmt.Println("Какую валюту вы хотите снять?\n1)Рубли\n2)Доллары\n3)Евро\n4)Назад")
-				fmt.Println()
-				fmt.Scan(&money)
-				if money == "1" {
-					fmt.Println("Сколько вы хотите снять?")
-					fmt.Scan(&quantity)
-					if rubblesBalance < quantity {
-						fmt.Println("Баланс не может быть отрицательным.")
-						fmt.Println()
-					} else {
-						rubblesBalance -= quantity
-						fmt.Printf("На вашем счету: %v рублей.\n", rubblesBalance)
-						fmt.Println()
-					}
-				} else if money == "2" {
-					fmt.Println("Сколько вы хотите снять?")
-					fmt.Scan(&quantity)
-					if dollarsBalance < quantity {
-						fmt.Println("Баланс не может быть отрицательным.")
-						fmt.Println()
-					} else {
-						dollarsBalance -= quantity
-						fmt.Printf("На вашем счету: %v долларов.\n", dollarsBalance)
-						fmt.Println()
-					}
-				} else if money == "3" {
-					fmt.Println("Сколько вы хотите снять?")
-					fmt.Scan(&quantity)
-					if euroBalance < quantity {
-						fmt.Println("Баланс не может быть отрицательным.")
-						fmt.Println()
-					} else {
-						euroBalance -= quantity
-						fmt.Printf("На вашем счету: %v евро.\n", euroBalance)
-						fmt.Println()
-					}
-				} else if money == "4" {
-					break
-				} else {
-					fmt.Println("Вы ввели что-то неверно, попробуйте еще раз.")
-					fmt.Println()
+				fmt.Println("Вы можете снять следующие валюты: Rub, Usd, Eur. Какую валюту вы хотите снять?")
+				fmt.Scanln(&transfer1)
+
+				transfer1 = strings.ToLower(transfer1)
+
+				if _, ok := bank[transfer1]; !ok {
+					fmt.Println("Вы ввели данные не верно.")
+					continue depositOut
+				}
+
+				fmt.Println("Сколько вы хотите снять?")
+				fmt.Scanln(&quantity)
+
+				if quantity < 0 || quantity > bank[transfer1] {
+					fmt.Println("Вы ввели неверное значение.")
+					continue depositOut
+				}
+
+				bank[transfer1] -= quantity
+
+				fmt.Printf("На вашем счету: %v %s.\n", float64(int(bank[transfer1]*100))/100.0, transfer1)
+
+				fmt.Println("Вернуться в главное меню? (Да/Нет)")
+				fmt.Scanln(&answer)
+
+				answer = strings.ToLower(answer)
+
+				if answer == "да" {
+					break depositOut
 				}
 			}
 		case 4:
 		convert:
 			for {
-				fmt.Println("Вы можете совершить следующие переводы:\n1)Перевод рублей в доллары\n2)Перевод рублей в евро\n3)Перевод долларов в рубли\n4)Перевод долларов в евро\n5)Перевод евро в рубли\n6)Перевод евро в доллары\n7)Назад")
-				fmt.Println()
-				fmt.Scan(&menu)
-				switch menu {
-				case 1:
-					fmt.Printf("У вас на балансе:\n%v рублей. Сколько вы хотите перевести?\n", rubblesBalance)
-					fmt.Scan(&quantity)
-					if rubblesBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						rubblesBalance -= quantity
-						dollarsBalance = quantity/97 + dollarsBalance
-						fmt.Printf("У вас на счету:\n%v рублей\n%v долларов\n", rubblesBalance, dollarsBalance)
-						fmt.Println()
-					}
-				case 2:
-					fmt.Printf("У вас на балансе:\n%v рублей. Сколько вы хотите перевести?\n", rubblesBalance)
-					fmt.Scan(&quantity)
-					if rubblesBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						rubblesBalance -= quantity
-						euroBalance = quantity/105 + euroBalance
-						fmt.Printf("У вас на счету:\n%v рублей\n%v евро\n", rubblesBalance, euroBalance)
-						fmt.Println()
-					}
-				case 3:
-					fmt.Printf("У вас на балансе:\n%v долларов. Сколько вы хотите перевести?\n", dollarsBalance)
-					fmt.Scan(&quantity)
-					if dollarsBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						dollarsBalance -= quantity
-						rubblesBalance = quantity*97 + rubblesBalance
-						fmt.Printf("У вас на счету:\n%v рублей\n%v долларов\n", rubblesBalance, dollarsBalance)
-						fmt.Println()
-					}
-				case 4:
-					fmt.Printf("У вас на балансе:\n%v долларов. Сколько вы хотите перевести?\n", dollarsBalance)
-					fmt.Scan(&quantity)
-					if dollarsBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						dollarsBalance -= quantity
-						euroBalance = quantity*1.1 + euroBalance
-						fmt.Printf("У вас на счету:\n%v долларов\n%v евро\n", dollarsBalance, euroBalance)
-						fmt.Println()
-					}
-				case 5:
-					fmt.Printf("У вас на балансе:\n%v евро. Сколько вы хотите перевести?\n", euroBalance)
-					fmt.Scan(&quantity)
-					if euroBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						euroBalance -= quantity
-						rubblesBalance = quantity*105 + rubblesBalance
-						fmt.Printf("У вас на счету:\n%v рублей\n%v евро\n", rubblesBalance, euroBalance)
-						fmt.Println()
-					}
-				case 6:
-					fmt.Printf("У вас на балансе:\n%v евро. Сколько вы хотите перевести?\n", euroBalance)
-					fmt.Scan(&quantity)
-					if euroBalance < quantity || quantity == 0 {
-						fmt.Println("Вы делаете что-то не так.")
-						fmt.Println()
-					} else {
-						euroBalance -= quantity
-						dollarsBalance = quantity/1.1 + dollarsBalance
-						fmt.Printf("У вас на счету:\n%v долларов\n%v евро\n", dollarsBalance, euroBalance)
-						fmt.Println()
-					}
-				case 7:
-					break convert
-				default:
-					fmt.Println("Что-то пошло не так.")
-					fmt.Println()
+				fmt.Println("Вы можете совершить переводы между Rub, Usd, Eur. Что во что вы хотите перевести?")
+				fmt.Scanln(&transfer1, &transfer2)
+
+				transfer1 = strings.ToLower(transfer1)
+				transfer2 = strings.ToLower(transfer2)
+
+				_, ok := bank[transfer1]
+				_, ok1 := bank[transfer2]
+				if !ok || !ok1 {
+					fmt.Println("Вы ввели данные не верно.")
+					continue convert
 				}
+
+				fmt.Println("Сколько вы хотите перевести?")
+				fmt.Scanln(&quantity)
+
+				bank[transfer1] -= quantity
+				if rubblesBalance < quantity || quantity == 0 {
+					fmt.Println("Вы делаете что-то не так.")
+					continue convert
+				}
+
+				sum = quantity * course[transfer1][transfer2]
+				bank[transfer2] += sum
+
+				fmt.Printf("У вас на счету:\n%v %s\n%v %s\n", bank[transfer1], transfer1, bank[transfer2], transfer2)
 			}
 		case 5:
 			break mainLoop
 		default:
 			fmt.Println("Что-то пошло не так.")
-			fmt.Println()
 		}
 	}
 }
