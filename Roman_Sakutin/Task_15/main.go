26402094b1858c8161a5a5787663fab2c9583b86
yuldash13 <yuldash.nazarov@icloud.com>
2024-11-11 22:49:47 +0300

    changed

--- Roman_Sakutin/Task_15/main.go
+++ Roman_Sakutin/Task_15/main.go
@@ -2,33 +2,18 @@ package main
 
 import "fmt"
 
+const attempt = 3
+
 func main() {
-	var attemptOne, attemptTwo, attemptThree string
+	var password string
 
-	fmt.Println("Введите пароль:\n(Решите ребус: ''Папа рок' ''соль)")
-
-	fmt.Scan(&attemptOne)
-	if attemptOne == "пароль" {
-		fmt.Println("Секртеная информация: продай все акции")
-		return
-	} else {
+	for i := 0; i < attempt; i++ {
+		fmt.Println("Введите пароль:\n(Решите ребус: ''Папа рок' ''соль)")
+		fmt.Scan(&password)
+		if password == "пароль" {
+			fmt.Println("Секртеная информация: продай все акции")
+			break
+		}
 		fmt.Println("Неверный пароль")
-		fmt.Scan(&attemptTwo)
-	}
-
-	if attemptTwo == "пароль" {
-		fmt.Println("Секртеная информация: продай все акции")
-		return
-	} else {
-		fmt.Println("Неверный пароль")
-		fmt.Scan(&attemptThree)
-	}
-
-	if attemptThree == "пароль" {
-		fmt.Println("Секртеная информация: продай все акции")
-		return
-	} else {
-		fmt.Println("Неверный пароль\nВы израсходавали все попытки, повторите попытку позже")
-		return
 	}
 }
