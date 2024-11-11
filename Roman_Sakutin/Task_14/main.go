359d65d376a3864b8097b205691e13f2ddf68636
yuldash13 <yuldash.nazarov@icloud.com>
2024-11-11 21:30:30 +0300

    changes

--- Roman_Sakutin/Task_14/main.go
+++ Roman_Sakutin/Task_14/main.go
@@ -2,30 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode/utf8"
 )
 
 func main() {
 	var (
 		name, symbol string
-		array        []rune
 	)
 	fmt.Println("Введите свое имя:")
 	fmt.Scan(&name)
 	fmt.Println("Введите символ:")
 	fmt.Scan(&symbol)
 
-	nameStr := symbol + " " + name + " " + symbol
+	nameStr := fmt.Sprintf("%v"+" "+name+" "+"%v", symbol, symbol)
 
 	lenght := utf8.RuneCountInString(nameStr)
 
-	symbolRune := rune(symbol[0])
-
-	for i := 0; i < lenght; i++ {
-		array = append(array, symbolRune)
-	}
-
-	lid := string(array)
+	lid := strings.Repeat(symbol, lenght)
 
 	fmt.Printf("%s\n%s\n%s", lid, nameStr, lid)
 }
