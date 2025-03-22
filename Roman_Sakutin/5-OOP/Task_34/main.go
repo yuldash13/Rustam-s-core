a39d39358c122e8d9cd72139b7086395576d339e
yuldash13 <yuldash.nazarov@icloud.com>
2025-03-23 00:26:50 +0300

    changes

--- Roman_Sakutin/5-OOP/Task_34/main.go
+++ Roman_Sakutin/5-OOP/Task_34/main.go
@@ -2,69 +2,86 @@ package main
 
 import (
 	"fmt"
+	"strings"
+)
+
+const (
+	z = 5
 )
 
 type Character struct {
-	name string
-	Drawer
-}
-
-type Drawer struct {
+	name   string
 	symbol string
 	x      int
 	y      int
 }
 
+type Drawer struct {
+	field [][]string
+}
+
+var info map[string]Character
+
 func main() {
-	field := [][]string{
-		{"|", "-", "-", "-", "-", "-", "|"},
-		{"|", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", "|"},
-		{"|", "-", "-", "-", "-", "-", "|"},
-	}
+	info = make(map[string]Character, 100)
+	drawer := NewDrawer()
+	drawer.initField()
 	for {
 		fmt.Println("Вы хотите создать нового персонажа?\n1)Да\n2)Нет")
 		var answer string
 		fmt.Scan(&answer)
-		if answer == "Нет" || answer == "нет" {
+		if strings.ToLower(answer) == "нет" {
 			break
-		} else if answer == "Да" || answer == "да" {
-			fmt.Println("Создайте своего персонажа:")
-			fmt.Println("1)Введите имя:\n2)Выберите обозначение персонажа (#/$/&):\n3)Введите свои координаты (x,y=1-5):")
-			showField(field)
-			var (
-				name, symbol string
-				x, y         int
-			)
-			fmt.Scan(&name, &symbol, &x, &y)
-			newDrawer := NewDrawer(symbol, x, y)
-			info := saveCharacter(NewCharacter(name,*newDrawer))
-			showCharacter(info, field)
+		} else if strings.ToLower(answer) == "да" {
+			character := createCharacter(*drawer)
+			character.saveCharacter()
+			fmt.Println("Вы хотите посмотреть созданных персонажей?\n1)Да\n2)Нет")
+			fmt.Scan(&answer)
+			if strings.ToLower(answer) == "нет" {
+				break
+			} else if strings.ToLower(answer) == "да" {
+				character.showCharacter(info, drawer)
+			}
 		} else {
 			fmt.Println("Вы ввели что-то неверно.")
 		}
 	}
 }
 
-func NewCharacter(name string, drawer Drawer) *Character {
-	return &Character{name: name, Drawer: drawer}
-}
-
-func NewDrawer(symbol string, x, y int) *Drawer{
-	return &Drawer{symbol: symbol, x: x, y: y}
-}
-
-func saveCharacter(c *Character) map[string]Character {
-	info := map[string]Character{
-		c.name: *c,
+func NewCharacter(name string, symbol string, x, y int) *Character {
+	return &Character{
+		name:   name,
+		symbol: symbol,
+		x:      x,
+		y:      y,
 	}
-	return info
 }
 
-func showCharacter(info map[string]Character, field [][]string) {
+func NewDrawer() *Drawer {
+	return &Drawer{}
+}
+
+func createCharacter(drawer Drawer) *Character {
+	fmt.Println("Создайте своего персонажа:")
+	fmt.Println("1)Введите имя:\n2)Выберите обозначение персонажа (#/$/&):\n3)Введите свои координаты (x,y=1-5):")
+	drawer.showField()
+	var (
+		name, symbol string
+		x, y         int
+	)
+	fmt.Scan(&name, &symbol, &x, &y)
+	if x > 5 || y > 5 || x < 1 || y < 1 {
+		fmt.Println("Значение координаты должно быть от 1 до 5.")
+		createCharacter(drawer)
+	}
+	return NewCharacter(name, symbol, x, y)
+}
+
+func (c *Character) saveCharacter() {
+	info[c.name] = *c
+}
+
+func (c *Character) showCharacter(info map[string]Character, drawer *Drawer) {
 	for {
 		fmt.Println("Ниже представлены все созданные персонажи. Кого хотите посмотреть?")
 		for key := range info {
@@ -76,12 +93,12 @@ func showCharacter(info map[string]Character, field [][]string) {
 		key, exists := info[person]
 		if exists {
 			fmt.Printf("Имя: %v\n", key.name)
-			fmt.Printf("Обозначение: %v\n", key.Drawer.symbol)
-			fmt.Printf("Координата x: %v\n", key.Drawer.x)
-			fmt.Printf("Координата y: %v\n", key.Drawer.y)
+			fmt.Printf("Обозначение: %v\n", key.symbol)
+			fmt.Printf("Координата x: %v\n", key.x)
+			fmt.Printf("Координата y: %v\n", key.y)
 
-			field[key.Drawer.y][key.Drawer.x] = key.Drawer.symbol
-			showField(field)
+			drawer.saveSym(key.symbol, key.x, key.y)
+			drawer.showField()
 		} else if person == "Никого" {
 			break
 		} else {
@@ -90,9 +107,24 @@ func showCharacter(info map[string]Character, field [][]string) {
 	}
 }
 
-func showField(field [][]string) {
-	for i := 0; i < len(field); i++ {
-		fmt.Println(field[i])
+func (d *Drawer) showField() {
+	for i := 0; i < len(d.field); i++ {
+		fmt.Println(d.field[i])
 	}
 }
 
+func (d *Drawer) initField() {
+	d.field = [][]string{
+		{"|", "-", "-", "-", "-", "-", "|"},
+		{"|", " ", " ", " ", " ", " ", "|"},
+		{"|", " ", " ", " ", " ", " ", "|"},
+		{"|", " ", " ", " ", " ", " ", "|"},
+		{"|", " ", " ", " ", " ", " ", "|"},
+		{"|", " ", " ", " ", " ", " ", "|"},
+		{"|", "-", "-", "-", "-", "-", "|"},
+	}
+}
+
+func (d *Drawer) saveSym(symbol string, x, y int) {
+	d.field[y][x] = symbol
+}
