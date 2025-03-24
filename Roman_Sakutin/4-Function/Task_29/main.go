9d8abb6e94ebb9846b3cba052573fe1f6e61034c
yuldash13 <yuldash.nazarov@icloud.com>
2025-03-25 02:07:07 +0300

    changes

--- Roman_Sakutin/4-Function/Task_29/main.go
+++ Roman_Sakutin/4-Function/Task_29/main.go
@@ -15,23 +15,25 @@ func main() {
 
 func initBar(barName string) {
 	var (
-		symbol   string
+		symbol   rune
 		quantity int
 	)
-	fmt.Printf("Введите значок который будет показывать %s:", barName)
-	fmt.Scan(&symbol)
-	fmt.Printf("Введите количество %s в процентах (максимум 100):", barName)
+	fmt.Printf("Введите значок который будет показывать %s:\n", barName)
+	fmt.Scanf("%c", &symbol)
+	fmt.Scanf("%c", &symbol)
+	fmt.Printf("Введите количество %s в процентах (максимум 100):\n", barName)
 	fmt.Scan(&quantity)
 
 	bar := showBar(symbol, quantity)
 	fmt.Printf("У вас %d %s:\n%s\n", quantity, barName, bar)
 }
 
-func showBar(symbol string, n int) string {
+func showBar(symbol rune, n int) string {
 	array := []rune{'_', '_', '_', '_', '_', '_', '_', '_', '_', '_'}
 	n /= 10
 	for i := 0; i < n; i++ {
-		array[i] = rune(symbol[0])
+		array[i] = symbol
 	}
-	return string(array)
+	str := string(array)
+	return str
 }
--- Roman_Sakutin/4-Function/Task_31/main.go
+++ Roman_Sakutin/4-Function/Task_31/main.go
@@ -5,69 +5,59 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
-	"strings"
 )
+
+type Person struct {
+	surname  string
+	name     string
+	lastname string
+	job      string
+}
 
 func main() {
 	var menu string
-	post := []string{"Инженер", "Программист", "Мама", "Врач"}
-	list := []string{"Назаров Юлдаш Русланович", "Назаров Рустам Русланович", "Атамуратова Ширин Розымбаевна", "Абдалов Артур Умарович"}
+	person1 := Person{
+		surname:  "Назаров",
+		name:     "Юлдаш",
+		lastname: "Русланович",
+		job:      "Инженер",
+	}
+	person2 := Person{
+		surname:  "Назаров",
+		name:     "Рустам",
+		lastname: "Русланович",
+		job:      "Программист",
+	}
+	person3 := Person{
+		surname:  "Атамуратова",
+		name:     "Ширин",
+		lastname: "Розымбаевна",
+		job:      "Мама",
+	}
+	person4 := Person{
+		surname:  "Абдалов",
+		name:     "Артур",
+		lastname: "Умарович",
+		job:      "Врач",
+	}
+	people := []Person{person1, person2, person3, person4}
 	for {
 		fmt.Println("Меню:\n1)Список\n2)Добавить\n3)Удалить\n4)Поиск\n5)Выход")
 		fmt.Scan(&menu)
 		clearConsol()
 		switch menu {
 		case "Список":
-			showList(list, post)
+			clearConsol()
+			showList(&people)
 		case "Добавить":
-		add:
-			for {
-				var lastName, name, lastLastName, newFullName, newPosition string
-				showList(list, post)
-				fmt.Println("Введите ФИО нового сотрудника через пробел:")
-				fmt.Scan(&lastName, &name, &lastLastName)
-				newFullName = lastName + " " + name + " " + lastLastName
-			checking:
-				for i := 0; ; i++ {
-					if list[i] == newFullName {
-						clearConsol()
-						fmt.Println("Такой сотрудник уже существует")
-						break checking
-					}
-					if i < len(list) {
-						list = addWorker(list, newFullName)
-						fmt.Println("Введите должность нового сотрудника:")
-						fmt.Scan(&newPosition)
-						post = addWorker(post, newPosition)
-						clearConsol()
-						break add
-					}
-				}
-			}
+			clearConsol()
+			addWorker(&people)
 		case "Удалить":
-		delete:
-			for {
-				var lastName, name, lastLastName, fullName string
-				showList(list, post)
-				fmt.Println("Введите ФИО сотрудника которого надо удалить:")
-				fmt.Scan(&lastName, &name, &lastLastName)
-				clearConsol()
-				fullName = lastName + " " + name + " " + lastLastName
-				for i := 0; i < len(list); i++ {
-					if list[i] == fullName {
-						list, post = deleteWorker(list, post, fullName)
-						break delete
-					}
-				}
-				fmt.Println("Такого сотрудника не существует.")
-			}
+			clearConsol()
+			deleteWorker(&people)
 		case "Поиск":
 			clearConsol()
-			var lastName string
-			fmt.Println("Введите фамилию сотрудника:")
-			fmt.Scan(&lastName)
-			newList, newPost := searchLastName(list, post, lastName)
-			showList(newList, newPost)
+			searchPerson(&people)
 		case "Выход":
 			clearConsol()
 			return
@@ -78,52 +68,71 @@ func main() {
 	}
 }
 
-func searchLastName(list, post []string, lastName string) ([]string, []string) {
-	var (
-		newList, newPost []string
-	)
-	for i := 0; i < len(list); i++ {
-		str := strings.Split(list[i], " ")
-		if str[0] == lastName {
-			newList = append(newList, list[i])
-			newPost = append(newPost, post[i])
+func searchPerson(people *[]Person) *Person {
+	var exPerson Person
+	fmt.Println("Введите фамилию сотрудника:")
+	fmt.Scan(&exPerson.surname)
+	var n int
+	for i, person := range *people {
+		if person.surname == exPerson.surname {
+			fmt.Printf("%d)%s %s %s - %s\n", i+1, person.surname, person.name, person.lastname, person.job)
+			n++
 		}
 	}
-	return newList, newPost
-}
-
-func addWorker(array []string, str string) []string {
-	var (
-		newArray = make([]string, 0, len(array)+1)
-	)
-	slice := array[:(len(array))]
-	newArray = append(newArray, slice...)
-	newArray = append(newArray, str)
-	return newArray
-}
-
-func deleteWorker(list, post []string, str string) ([]string, []string) {
-	var (
-		newList = make([]string, 0, len(list)-1)
-		newPost = make([]string, 0, len(post)-1)
-	)
-	for i := 0; i < len(list); i++ {
-		if list[i] == str {
-			continue
-		}
-		newList = append(newList, list[i])
-		newPost = append(newPost, post[i])
+	if n > 0 {
+		return &Person{}
+	} else if n == 0 {
+		fmt.Println("Такого сотрудника не существует")
 	}
-
-	return newList, newPost
+	return &Person{}
 }
 
-func showList(list, post []string) {
+func addWorker(people *[]Person) *Person {
+	var newPerson Person
+	showList(people)
+	fmt.Println("Введите ФИО нового сотрудника через пробел:")
+	fmt.Scan(&newPerson.surname, &newPerson.name, &newPerson.lastname)
+	fmt.Println("Введите должность нового сотрудника:")
+	fmt.Scan(&newPerson.job)
+	for _, person := range *people {
+		if person == newPerson {
+			fmt.Println("Такой сотрудник уже существует")
+			return &Person{}
+		}
+	}
+	*people = append(*people, newPerson)
+	return &Person{}
+}
+
+func deleteWorker(people *[]Person) *Person {
+	var oldPerson Person
+	showList(people)
+	fmt.Println("Введите ФИО сотрудника которого надо удалить:")
+	fmt.Scan(&oldPerson.surname, &oldPerson.name, &oldPerson.lastname)
+	fmt.Println("Введите должность сотрудника:")
+	fmt.Scan(&oldPerson.job)
+	var n int
+	for i, person := range *people {
+		if person == oldPerson {
+			*people = append((*people)[:i], (*people)[i+1:]...)
+			n++
+		}
+	}
+	if n > 0 {
+		return &Person{}
+	} else if n == 0 {
+		fmt.Println("Такого сотрудника не существует")
+	}
+	return &Person{}
+}
+
+func showList(people *[]Person) {
 	fmt.Println("Список сотрудников:")
-	for i := 0; i < len(list); i++ {
-		fmt.Printf("%d)%s - %s\n", i+1, list[i], post[i])
+	for i, person := range *people {
+		fmt.Printf("%d)%s %s %s - %s\n", i+1, person.surname, person.name, person.lastname, person.job)
 	}
 }
+
 func clearConsol() {
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("cmd", "/c", "cls")
