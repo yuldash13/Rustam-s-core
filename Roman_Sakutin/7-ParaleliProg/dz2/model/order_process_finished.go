a8393ebea37c01705b0d2ff6efd0522e706aaea9
yuldash13 <yuldash.nazarov@icloud.com>
2025-09-21 03:17:34 +0300

    fuck

--- Roman_Sakutin/7-ParaleliProg/dz2/model/order_process_finished.go
+++
@@ -1,9 +0,0 @@
-package model
-
-func NewOrderProcessFinished(orderFinishedExternalInteraction OrderFinishedExternalInteraction, orderStates []OrderState, error error) *OrderProcessFinished {
-	return &OrderProcessFinished{
-		OrderFinishedExternalInteraction: orderFinishedExternalInteraction,
-		OrderStates:                      orderStates,
-		Error:                            error,
-	}
-}
--- Roman_Sakutin/7-ParaleliProg/dz2/model/order_process_started.go
+++
@@ -1,9 +0,0 @@
-package model
-
-func NewOrderProcessStarted(orderInitialized OrderInitialized, orderStates []OrderState, error error) *OrderProcessStarted {
-	return &OrderProcessStarted{
-		OrderInitialized: orderInitialized,
-		OrderStates:      orderStates,
-		Error:            error,
-	}
-}
